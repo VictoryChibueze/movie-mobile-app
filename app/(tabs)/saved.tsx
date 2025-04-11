307e1e1c794b1aa1ca9f9db9f29f160a131da2eb
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-04-11 09:53:31 +0100

    .

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,7 +1,11 @@
 import { StyleSheet, View, Text } from "react-native";
 
 const Saved = () => {
-  return <View className="flex-1 bg-[#000]"></View>;
+  return (
+    <View className="flex-1 bg-[#000]">
+      <Text className="text-white mt-10">yooo</Text>
+    </View>
+  );
 };
 
 export default Saved;
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,9 +1,33 @@
-import { StyleSheet, View, Text } from "react-native";
+import MovieCard from "@/components/movieCard";
+import { images } from "@/constants/images";
+import useFetch from "@/services/usefetch";
+import { fetchMovies } from "@/services/api";
+import { useRouter } from "expo-router";
+import { StyleSheet, View, Text, Image, FlatList } from "react-native";
 
 const Search = () => {
-  return <View className="flex-1 bg-[#000]"></View>;
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
+  return (
+    <View className="flex-1 bg-[#000]">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      ></Image>
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+      />
+      <Text>gooo</Text>
+    </View>
+  );
 };
 
 export default Search;
-
-const styles = StyleSheet.create({});
