a3fb1fec42be12816a7fb5754f0e7eb301fbe168
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-04-11 12:01:04 +0100

    .

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -2,7 +2,6 @@ import MovieCard from "@/components/movieCard";
 import { images } from "@/constants/images";
 import useFetch from "@/services/usefetch";
 import { fetchMovies } from "@/services/api";
-import { useRouter } from "expo-router";
 import {
   StyleSheet,
   View,
@@ -13,16 +12,30 @@ import {
 } from "react-native";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import { useEffect, useState } from "react";
 
 const Search = () => {
-  const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState("");
 
   const {
     data: movies,
     loading,
     error,
-  } = useFetch(() => fetchMovies({ query: "" }));
+    refetch: loadMovies,
+    reset,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
 
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+      } else {
+        reset();
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
   return (
     <View className="flex-1 bg-[#000]">
       <Image
@@ -35,6 +48,17 @@ const Search = () => {
         renderItem={({ item }) => <MovieCard {...item} />}
         keyExtractor={(item) => item.id.toString()}
         numColumns={3}
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className="mt-10 px-5">
+              <Text className="text-center text-gray-500">
+                {searchQuery.trim()
+                  ? "No movies found.."
+                  : "Search for a movie"}
+              </Text>
+            </View>
+          ) : null
+        }
         columnWrapperStyle={{
           gap: 16,
           justifyContent: "center ",
@@ -47,7 +71,11 @@ const Search = () => {
               <Image source={icons.logo} className="w-12 h-10" />
             </View>
             <View className="my-5">
-              <SearchBar placeholder="Search movies...." />
+              <SearchBar
+                placeholder="Search movies...."
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)}
+              />
             </View>
 
             {loading && (
@@ -62,15 +90,12 @@ const Search = () => {
               <Text className="text-red-500 px-5">Error:{error.message}</Text>
             )}
 
-            {!loading &&
-              !error &&
-              "SEARCH TERM".trim() &&
-              movies?.length > 0 && (
-                <Text className="text-xl text-white font-bold">
-                  Search Results for:{" "}
-                  <Text className="text-gray-400 text-xs">SEARCH TERM</Text>
-                </Text>
-              )}
+            {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
+              <Text className="text-xl text-white font-bold">
+                Search Results for:{" "}
+                <Text className="text-gray-400 text-xs">{searchQuery}</Text>
+              </Text>
+            )}
           </>
         }
       />
