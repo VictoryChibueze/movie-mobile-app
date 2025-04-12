7e28f52b71fc4fe944b1fcca0b8b570d9a1d57fe
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-04-12 07:24:24 +0100

    .

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -3,6 +3,21 @@ import useFetch from "@/services/usefetch";
 import { useLocalSearchParams } from "expo-router";
 import { View, Text, ScrollView, Image, StatusBar } from "react-native";
 import { icons } from "@/constants/icons";
+
+interface MovieInfoProps {
+  label: string;
+  value?: string | number | null;
+}
+const MovieInfo = ({ label, value }: MovieInfoProps) => {
+  return (
+    <View className="flex-col items-start justify-center mt-5">
+      <Text className="text-white font-normal text-sm">{label}</Text>
+      <Text className="text-white font-bold text-sm mt-2">
+        {value || "N/A"}
+      </Text>
+    </View>
+  );
+};
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
 
@@ -30,9 +45,16 @@ const MovieDetails = () => {
                 {"  "}
                 <Text className="text-gray text-sm ">{movie?.runtime}m</Text>
               </Text>
-              <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1">
+              <View className="flex-col items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1">
                 <Image source={icons.star} className="size-4" />
+                <Text className="text-white font-bold text-sm">
+                  {Math.round(movie?.vote_average ?? 0)}/10
+                </Text>
+                <Text className="text-light text-sm">
+                  ({movie?.vote_count}votes)
+                </Text>
               </View>
+              <MovieInfo label="Overview" value={movie?.overview} />
             </View>
           </View>
         </View>
