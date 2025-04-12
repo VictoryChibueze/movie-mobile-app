db35f1912355029fcbb709600420e4c01ec83e0d
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-04-12 05:39:35 +0100

    .

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -2,7 +2,7 @@ import { fetchMoviesDetails } from "@/services/api";
 import useFetch from "@/services/usefetch";
 import { useLocalSearchParams } from "expo-router";
 import { View, Text, ScrollView, Image, StatusBar } from "react-native";
-
+import { icons } from "@/constants/icons";
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
 
@@ -21,7 +21,20 @@ const MovieDetails = () => {
             className="w-full h-[550px]"
             resizeMode="stretch"
           />
-          <View className=""></View>
+          <View className="flex-col items-start justify-center mt-5 px-5">
+            <Text className="text-white text-xl">{movie?.title}</Text>
+
+            <View className="flex-row items-center gap-x-1 mt-2">
+              <Text className="text-gray-400 text-sm">
+                {movie?.release_date?.split("-")[0]}
+                {"  "}
+                <Text className="text-gray text-sm ">{movie?.runtime}m</Text>
+              </Text>
+              <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1">
+                <Image source={icons.star} className="size-4" />
+              </View>
+            </View>
+          </View>
         </View>
       </ScrollView>
     </View>
